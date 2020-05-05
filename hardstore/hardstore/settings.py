e03e0d54571f0f9d2420eda9241a828f78b3ab4c
Agustin Domínguez  (PC) <agussebastiandominguez@gmail.com>
2020-05-05 17:09:47 -0300

    Instalé users
    App aparte para el login in/sign in

--- hardstore/hardstore/settings.py
+++ hardstore/hardstore/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    ''
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'market.apps.MarketConfig'
+    'market.apps.MarketConfig',
+    'users.apps.UsersConfig'
 ]
 
 MIDDLEWARE = [
