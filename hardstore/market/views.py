ab3bd31b5c3efaa016dbad56cb16103bd604e564
José Garay <josemgaray14@gmail.com>
2020-05-06 13:52:46 -0300

    prueba

--- hardstore/market/views.py
+++ hardstore/market/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
+
 def home(request):
 
-	return render(request,'home.html',{'titulo':'Home'})
+    return render(request, 'home.html', {'titulo': 'Home'})
 
 # Create your views here.
+
+
+def login(request):
+    return render(request, 'LogIn.html')
