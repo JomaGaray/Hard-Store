cc184bef4bd951633a258776bd390345d271fb4a
Agustin Domínguez  (PC) <agussebastiandominguez@gmail.com>
2020-05-06 17:03:00 -0300

    Borre modelo producto
    Actualizacion pycache

--- hardstore/market/models.py
+++ hardstore/market/models.py
@@ -1,11 +1,5 @@
 from django.db import models
 from django.utils import timezone
 
-class Producto(models.Model):
-	nombre = models.CharField(max_length = 50)
-	descripcion = models.TextField()
-	precio = models.
-	imagen = models.
-	fecha_publicacion = models.
 
 # Create your models here.
