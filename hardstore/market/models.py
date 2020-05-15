6a5b3f45dd681de8a86a8192238acb35acd94b5b
Agustin Domínguez  (PC) <agussebastiandominguez@gmail.com>
2020-05-15 08:46:27 -0300

    Agrego modelos y la primer migracion

--- hardstore/market/models.py
+++ hardstore/market/models.py
@@ -1,5 +1,46 @@
 from django.db import models
 from django.utils import timezone
 
+#class Image(models.Model):
+#class Like(models.Model):
+#class Cart(models.Model):
+#class (models.Model):
+#class (models.Model):
 
-# Create your models here.
+class Category(models.Model):
+	name = models.CharField(max_length=200, null=True)
+
+	def __str__(self):
+		return self.name
+
+class Product(models.Model):
+	name = models.CharField(max_length=200, null=True)
+	description = models.CharField(max_length=200, null=True)
+	price = models.FloatField(null=True)
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
+	category = models.ManyToManyField(Category)
+
+	def __str__(self):
+		return self.name
+
+class Customer(models.Model):
+	name = models.CharField(max_length=200, null=True)
+	email = models.CharField(max_length=200, null=True)
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+	def __str__(self):
+		return self.name
+
+
+class Order(models.Model):
+	STATUS = (	('Pendiente','Pendiente'),
+				('Confirmada','Confirmada'),
+				('Cancelada','Cancelada')
+		)
+	#referencia a product
+	product = models.ForeignKey(Product, null=True, on_delete = models.SET_NULL)
+	#referencia a customer
+	customer = models.ForeignKey(Customer, null=True, on_delete = models.SET_NULL)
+
+	status = models.CharField(max_length=200, null=True, choices=STATUS)
+
