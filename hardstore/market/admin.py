a3d8aff46381de6bef7fa19e41de606e622027cf
Agustin Domínguez  (PC) <agussebastiandominguez@gmail.com>
2020-05-20 23:22:15 -0300

    Cambios modelo Oferta y view de home

--- hardstore/market/admin.py
+++ hardstore/market/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import Group
-from .models import Product,Customer,Order,Category
+from .models import Product,Customer,Order,Category,Oferta
 
 admin.site.unregister(Group)
 admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(Category)
+admin.site.register(Oferta)
--- hardstore/market/views.py
+++ hardstore/market/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpRequest
-from .models import Category,Product,Order,Customer
+from .models import Category,Product,Order,Customer,Oferta
 from django.views import View
 import datetime
 
 class VistaHome(View):
 	def get(self,request):
 		products = Product.objects.all()
-		#ofertas = Oferta.objects.filter()
+		ofertas = Oferta.objects.all() #tengo que filtrar 6 ofertas
 		# por ahora los destacados van a ser los productos cargados mas recientemente
 		#destacados = Product.objects.filter()
 		
 		context = {
 		'titulo':'Home',
-		'products' : products#,
-		#'ofertas' : ofertas ,  # hago una query de 6 productos en oferta
+		'products' : products,
+		'ofertas' : ofertas #,  # hago una query de 6 productos en Oferta
 		#'destacados' : destacados  # hago una query de los 6 mas destacados
 		}
 		return render(request, 'home.html', context)
