244f319e1c914d1fda5a6a7add876417d929c544
Agustin Domínguez  (PC) <agussebastiandominguez@gmail.com>
2020-05-22 06:05:31 -0300

    Añado Cliente a admin para verificar

--- hardstore/market/views.py
+++ hardstore/market/views.py
@@ -2,20 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse,HttpRequest
 from .models import Categoria,Producto,Orden,Cliente,Oferta
 from django.views import View
-import datetime
+
 
 class VistaHome(View):
 	def get(self,request):
 		productos = Producto.objects.all()
 		ofertas = Oferta.objects.all() #tengo que filtrar 6 ofertas
+
 		# por ahora los destacados van a ser los productos cargados mas recientemente
+		# hago una query de los 6 mas destacados
+		
 		#destacados = Product.objects.filter()
+
 		
 		context = {
 		'titulo':'Home',
 		'productos' : productos,
-		'ofertas' : ofertas #,  # hago una query de 6 productos en Oferta
-		#'destacados' : destacados  # hago una query de los 6 mas destacados
+		'ofertas' : ofertas 
+		#'destacados' : destacados  
 		}
 		return render(request, 'home.html', context)
 	
