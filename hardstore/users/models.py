16fe33dd3c081e479b8f987a2bd4ebe45cc82839
Agustin Domínguez  (PC) <agussebastiandominguez@gmail.com>
2020-05-25 12:56:00 -0300

    Agrego cliente a models de users

--- hardstore/users/models.py
+++ hardstore/users/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
-# class Usuario (models.Model)
+
+class Cliente(models.Model):
+	usuario = models.OneToOneField(User , null = True , on_delete = models.CASCADE)
+	f_creacion = models.DateTimeField(auto_now_add = True ,null = True)
+
+	def __str__(self):
+		return self.usuario.first_name +' '+ self.usuario.last_name
