1c72f2b9742fedfe0079749595a2c3ce292b9159
Agustin Domínguez  (PC) <agussebastiandominguez@gmail.com>
2020-05-25 13:25:56 -0300

    Saco el registro de Cliente de market.admin y lo paso a users.admin

--- hardstore/market/admin.py
+++ hardstore/market/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import Group
-from .models import Producto,Cliente,Orden,Categoria,Oferta
+from .models import Producto,Orden,Categoria,Oferta
 
 admin.site.unregister(Group)
 admin.site.register(Producto)
 admin.site.register(Orden)
 admin.site.register(Categoria)
 admin.site.register(Oferta)
-admin.site.register(Cliente)
