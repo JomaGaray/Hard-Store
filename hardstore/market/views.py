10b32f06edd744a8b273a8fbbcef01b501084951
Agustin Domínguez  (PC) <agussebastiandominguez@gmail.com>
2020-05-28 20:35:19 -0300

    Borro las viejas implementaciones de views

--- hardstore/market/views.py
+++ hardstore/market/views.py
@@ -19,25 +19,6 @@ class VistaHome(TemplateView):
 		context['titulo'] = 'home'
 		return context
 
-#Implementacion anterior
-#class VistaHome(View):
-#	def get(self,request):
-#		productos = Producto.objects.all()
-#		ofertas = Oferta.objects.all()[:6] #tengo que filtrar 6 ofertas
-#
-#		# por ahora los destacados van a ser los productos cargados mas recientemente
-#		# hago una query de los 6 mas destacados
-#		
-#		destacados = Producto.objects.all()[:6]
-#
-#		
-#		context = {
-#		'titulo':'Home',
-#		'productos' : productos,
-#		'ofertas' : ofertas ,
-#		'destacados' : destacados  
-#		}
-#		return render(request, 'home.html', context)
 
 
 ################ VISTA DE UN PRODUCTO ################
@@ -75,17 +56,6 @@ class VistaUnProducto(TemplateView):
 								#y le asigno una queryset para que seleccione el producto especifico 
 		return context
 
-#Implementacion anterior	
-#class VistaUnProducto(View):
-#	def get(self,request, pk_producto):
-#		#url dinamico, solo busco un producto en particular 
-#		producto= Producto.objects.get(id = pk_producto)
-#		
-#		context={
-#			'producto':producto,
-#		}
-#		
-#		return render(request, 'producto.html', context)
 
 #################### VISTA CREATE,READ,UPDATE,DELETE ########################
 
