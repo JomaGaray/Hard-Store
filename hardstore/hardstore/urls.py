161cf96291632b49baefa2e9fd61ee09440ab292
José Garay <josemgaray14@gmail.com>
2020-05-28 13:27:34 -0300

    Cambio nombre en urls

--- hardstore/hardstore/urls.py
+++ hardstore/hardstore/urls.py
@@ -24,11 +24,13 @@ urlpatterns = [
     # path('', include('market.urls')), manera menos directa -Joma
     path('login/', user_views.VistaLogin.as_view(), name='login'),
     path('signup/', user_views.VistaSignup.as_view(), name='signup'),
-  
-    #path para productos de una categoria en particular
-    path('productos/<str:categoria>/', market_views.ProductosList.as_view(), name='productosCategoria'),
-  
-  #path para un prodcuto en particular, un path dinamico <str:pk_prod>
-    path('producto/<str:pk_producto>/', market_views.VistaUnProducto.as_view(), name='producto'),
+
+    # path para productos de una categoria en particular
+    path('productos/<str:categoria>/',
+         market_views.VistaMuchosProductos.as_view(), name='productosCategoria'),
+
+    # path para un prodcuto en particular, un path dinamico <str:pk_prod>
+    path('producto/<str:pk_producto>/',
+         market_views.VistaUnProducto.as_view(), name='producto'),
     path('', market_views.VistaHome.as_view(), name='home'),
 ]
