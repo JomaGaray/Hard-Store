e54ec7990ac61935ead50647244ce873446573df
Agustin Domínguez  (PC) <agussebastiandominguez@gmail.com>
2020-06-09 16:53:33 -0300

    Nuevo form

--- hardstore/market/forms.py
+++ hardstore/market/forms.py
@@ -1,9 +1,21 @@
 from django.forms import ModelForm
-from .models import Producto
+from .models import Producto,Categoria,ImagenProducto
 
 class ProductoForm(ModelForm):
 
 	class Meta:
 		model = Producto
-		fields = ('nombre','descripcion','precio','categoria','imagen')
-		
+		fields = ('nombre','descripcion','precio','categoria')
+
+class ImagenForm(ModelForm):
+
+	class Meta:
+		model = ImagenProducto
+		fields = ('imagen',)
+
+class CategoriaForm(ModelForm):
+
+	class Meta:
+		model = Categoria
+		fields = ('nombre',)
+
