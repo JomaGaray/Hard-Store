f78711bc0a342b882ee234392e46d951029a9ecc
José Garay <josemgaray14@gmail.com>
2020-06-14 17:19:37 -0300

    Cambio en models

--- hardstore/users/models.py
+++ hardstore/users/models.py
@@ -2,37 +2,14 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 class UserProfile(models.Model):
-	user = models.OneToOneField(User , null = True , on_delete = models.CASCADE, related_name='profile')
-	f_creacion = models.DateTimeField(auto_now_add = True ,null = True)
-	birth_date = models.DateField(null=True, blank=True)
-	
-	def __str__(self):
-		return self.user.username
+    user = models.OneToOneField(
+        User, null=True, on_delete=models.CASCADE, related_name='profile')
+    first_name = models.CharField(max_length=200, null=True, blank=True)
+    last_name = models.CharField(max_length=200, null=True, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    f_creacion = models.DateTimeField(auto_now_add=True, null=True)
 
-#Utilizando signals
-#De esta manera, cuando creamos una instancia de User, se crea tambien una instancia de Profile asociada a esta 
-	@receiver(post_save, sender=User)
-	def update_profile_signal(sender, instance, created, **kwargs):
-		if (created):
-			UserProfile.objects.create(user=instance)
-
-#instance.Userprofile.save()
-
-	#def __str__(self):
-	#	return self.usuario.User.first_name +' '+ self.usuario.last_name
-		
-#Now this is where the magic happens: we will now define signals so 
-#our Profile model will be automatically created/updated when we create/update User instances.
-
-#@receiver(post_save, sender=User)
-#def create_user_profile(sender, instance, created, **kwargs):
-	if created:
-		Profile.objects.create( user=instance )
-
-#@receiver(post_save, sender=User)
-#def save_user_profile(sender, instance, **kwargs):
-	instance.profile.save()
+    def __str__(self):
+        return str(self.first_name)
