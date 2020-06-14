c4a2cdbba04a6dc9bdd9954981b8088a584b5d5d
José Garay <josemgaray14@gmail.com>
2020-06-14 17:17:12 -0300

    Modificacion en views de market, reemplace "cliente" por "UserProfile"

--- hardstore/market/views.py
+++ hardstore/market/views.py
@@ -1,31 +1,30 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpRequest
-from .models import Categoria,Producto,Orden,Cliente,Oferta
+from .models import Categoria, Producto, Orden, UserProfile, Oferta
 from .forms import ProductoForm
 from django.views.generic import View, ListView, TemplateView, DetailView
 
 
 ################ VISTA DEL HOME ################
 
-#Implementacion con TemplateView
+# Implementacion con TemplateView
 class VistaHome(TemplateView):
-	template_name = 'home.html'
-	
-	def get_context_data(self,**kwargs):
-		context = super().get_context_data(**kwargs)
-		context['productos'] = Producto.objects.all()
-		context['ofertas'] = Oferta.objects.all()[:6]
-		context['destacados'] = Producto.objects.all()[:6]
-		context['titulo'] = 'home'
-		# traigo todas las categorias
-		context['categorias'] = Categoria.objects.all()
-		return context
+    template_name = 'home.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['productos'] = Producto.objects.all()
+        context['ofertas'] = Oferta.objects.all()[:6]
+        context['destacados'] = Producto.objects.all()[:6]
+        context['titulo'] = 'home'
+        # traigo todas las categorias
+        context['categorias'] = Categoria.objects.all()
+        return context
 
 
 ################ VISTA DE UN PRODUCTO ################
 
-#vista para muchos productos de una categoria en particular 
+# vista para muchos productos de una categoria en particular
 class VistaMuchosProductos(ListView):
     template_name = 'productosCategoria.html'
     model = Producto
@@ -42,15 +41,16 @@ class VistaMuchosProductos(ListView):
 
 ################ VISTA DE UN PRODUCTO ###################
 
-class VistaUnProducto(DetailView):
-	template_name = 'producto.html'
-	queryset = Producto.objects.all()
-	
-	def get_object(self):
-		id_ = self.kwargs.get('pk_producto') #argumento pasado por el url
-		return get_object_or_404(Producto, id=id_)
 
-#Implementacion vieja con TemplateView
+class VistaUnProducto(DetailView):
+    template_name = 'producto.html'
+    queryset = Producto.objects.all()
+
+    def get_object(self):
+        id_ = self.kwargs.get('pk_producto')  # argumento pasado por el url
+        return get_object_or_404(Producto, id=id_)
+
+# Implementacion vieja con TemplateView
 # #class VistaUnProducto(TemplateView):
 #	template_name = 'producto.html'
 
@@ -58,44 +58,44 @@ class VistaUnProducto(DetailView):
 #		context = super().get_context_data(**kwargs) #obtiene los datos del modelo, en este caso "Producto"
 #		pk_producto = kwargs['pk_producto'] #tomo el argumento del url que indica el numero id del producto
 #		context['producto'] = Producto.objects.get(id = pk_producto) #añado otro field al modelo
-								#y le asigno una queryset para que seleccione el producto especifico 
+        # y le asigno una queryset para que seleccione el producto especifico
 #		return context
-
 
 
 
 #################### VISTA CREATE,READ,UPDATE,DELETE ########################
 
-#ESTA VISTA DEBE SER IMPLEMENTADA SOLAMENTE PARA LOS ADMINISTRADORES
+# ESTA VISTA DEBE SER IMPLEMENTADA SOLAMENTE PARA LOS ADMINISTRADORES
 
 class VistaCRUDProducto(View):
 
-	def CrearProducto(self,request):
-		form = ProductoForm()
-		if request.method == 'POST':
-			form = ProductoForm(request.POST)
-			if form.is_valid():
-				form.save()
-				# post.user = request.user - A futuro para saber que administrador realizo esta accion
-				return redirect('/') #redirecciona a la lista de productos
-		return render(request,'producto_form.html',{'producto':form})
+    def CrearProducto(self, request):
+        form = ProductoForm()
+        if request.method == 'POST':
+            form = ProductoForm(request.POST)
+            if form.is_valid():
+                form.save()
+                # post.user = request.user - A futuro para saber que administrador realizo esta accion
+                return redirect('/')  # redirecciona a la lista de productos
+        return render(request, 'producto_form.html', {'producto': form})
 
-	def ModProducto(self,request,id):
-		producto = get_object_or_404(Producto,pk=id)
-		form = ProductoForm(instance=producto)
-		if request.method == 'POST':
-			form = ProductoForm(request.POST, request.FILES, instance=producto) #request.FILES es debido a las imagenes
-			if form.is_valid():
-				form.save()
-				return redirect('/') #redirecciona a la lista de productos
-		return render(request,'producto_form.html',{'producto':form})
+    def ModProducto(self, request, id):
+        producto = get_object_or_404(Producto, pk=id)
+        form = ProductoForm(instance=producto)
+        if request.method == 'POST':
+            # request.FILES es debido a las imagenes
+            form = ProductoForm(request.POST, request.FILES, instance=producto)
+            if form.is_valid():
+                form.save()
+                return redirect('/')  # redirecciona a la lista de productos
+        return render(request, 'producto_form.html', {'producto': form})
 
-	def EliminarProducto(self,request,id):
-		#Producto.objecets.filter(pk=id).delete()
-		producto = get_object_or_404(Producto,pk=id)
-		if request.method == 'POST':
-			producto.delete()
-			return redirect('/') #redirecciona a la lista de productos
-		args = {'producto':producto}
-		# redirecciona a una lista de productos que NO va a ser la misma de los clientes
-		return render(request, 'eliminar_producto.html' , args)
+    def EliminarProducto(self, request, id):
+        # Producto.objecets.filter(pk=id).delete()
+        producto = get_object_or_404(Producto, pk=id)
+        if request.method == 'POST':
+            producto.delete()
+            return redirect('/')  # redirecciona a la lista de productos
+        args = {'producto': producto}
+        # redirecciona a una lista de productos que NO va a ser la misma de los clientes
+        return render(request, 'eliminar_producto.html', args)
