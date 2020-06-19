496403fe07c7392bf50714234a4e6f3949fe44a8
José Garay <josemgaray14@gmail.com>
2020-06-18 21:17:02 -0300

    Agrego modelo de Admin con permisos

--- hardstore/users/models.py
+++ hardstore/users/models.py
@@ -6,10 +6,23 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(
         User, null=True, on_delete=models.CASCADE, related_name='profile')
-    first_name = models.CharField(max_length=200, null=True, blank=True)
-    last_name = models.CharField(max_length=200, null=True, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
     f_creacion = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return str(self.first_name)
+
+
+class AdminProfile(models.Model):
+    user = models.OneToOneField(
+        User, null=True, on_delete=models.CASCADE)
+    f_creacion = models.DateTimeField(auto_now_add=True, null=True)
+
+    class Meta:
+        permissions = [
+            ("crear_producto", "Puede crear un producto"),
+            ("modificar_producto", "Puede modificar un producto"),
+            ("eliminar_producto", "Puede eliminar un producto"),
+        ]
 
     def __str__(self):
         return str(self.first_name)
