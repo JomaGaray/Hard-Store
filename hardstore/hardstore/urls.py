1aef56780818c0b2ab747e038b24d5e77c8da057
José Garay <josemgaray14@gmail.com>
2020-06-19 23:30:49 -0300

    Agrego path

--- hardstore/hardstore/urls.py
+++ hardstore/hardstore/urls.py
@@ -25,7 +25,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', market_views.index.as_view(), name='home'),
-    
+
     path('accounts/', include('django.contrib.auth.urls')),
     path('signup/', user_views.UserSignUpView.as_view(), name='signup'),
     # path('', include('market.urls')), manera menos directa -Joma
@@ -33,37 +33,50 @@ urlpatterns = [
     #path('signup/', user_views.VistaSignup.as_view(), name='signup'),
 
     path('search/', market_views.SearchView.as_view(), name='search'),
-  
-    # path para un prodcuto en particular DEL LADO DEL CLIENTE, un path dinamico 
+
+    # path para un prodcuto en particular DEL LADO DEL CLIENTE, un path dinamico
     #path('producto/<int:pk_producto>/', market_views.VistaUnProducto.as_view(), name='producto'),
 
-    path('producto/<int:pk_producto>/', market_views.ProductoDetail.as_view(), name='producto-detalle'),
+    path('producto/<int:pk_producto>/',
+         market_views.ProductoDetail.as_view(), name='producto-detalle'),
 
-    path('productos_categoria/<int:pk_categoria>', market_views.ProductosCategoriaList.as_view(), name='productos-categoria-list'),
+    path('productos_categoria/<int:pk_categoria>',
+         market_views.ProductosCategoriaList.as_view(), name='productos-categoria-list'),
 
 
 
-    #PATH CRUD hay que pasarlo cuando creemos todo la parte de administradores
-    #crear un producto
-    path('crear_producto/', market_views.ProductoCreate.as_view() , name='crear_producto'),
+    # PATH CRUD hay que pasarlo cuando creemos todo la parte de administradores
+    # crear un producto
+    path('crear_producto/', market_views.ProductoCreate.as_view(),
+         name='crear_producto'),
 
-    #modificar un producto
+    # modificar un producto
     #path('modificar_producto/<int:id>/', market_views.VistaCRUDProducto.ModProducto , name='modificar_producto'),
-    path('modificar_producto/<int:pk>/', market_views.ProductoUpdate.as_view(), name='modificar_producto'),
+    path('modificar_producto/<int:pk>/',
+         market_views.ProductoUpdate.as_view(), name='modificar_producto'),
 
-    #eliminar un producto
-    path('eliminar_producto/<int:pk>/', market_views.ProductoDelete.as_view() , name='eliminar_producto'),
+    # eliminar un producto
+    path('eliminar_producto/<int:pk>/',
+         market_views.ProductoDelete.as_view(), name='eliminar_producto'),
 
-    #PATH CRUD para Categorias
+    # PATH CRUD para Categorias
 
-    #crear una Categoria
-    path('crear_categoria/', market_views.CategoriaCreate.as_view() , name='crear_categoria'),
+    # crear una Categoria
+    path('crear_categoria/', market_views.CategoriaCreate.as_view(),
+         name='crear_categoria'),
 
-    #modificar un Categoria
-    path('modificar_categoria/<int:pk>/', market_views.CategoriaUpdate.as_view() , name='modificar_categoria'),
+    # modificar un Categoria
+    path('modificar_categoria/<int:pk>/',
+         market_views.CategoriaUpdate.as_view(), name='modificar_categoria'),
 
-    #eliminar un Categoria
-    path('eliminar_categoria/<int:pk>/', market_views.CategoriaDelete.as_view() , name='eliminar_categoria'),
+    # eliminar un Categoria
+    path('eliminar_categoria/<int:pk>/',
+         market_views.CategoriaDelete.as_view(), name='eliminar_categoria'),
+
+
+    # crear admin, path de prueba
+    path('crearAdmin/', user_views.AdminSignUpView.as_view(), name='crear_Admin'),
+
 
 ]
 
