df807e462c5bdd6bd7b59f161774ab0944496cd5
José Garay <josemgaray14@gmail.com>
2020-06-19 23:32:47 -0300

    Agrego cosas en signals

--- hardstore/users/signals.py
+++ hardstore/users/signals.py
@@ -1,4 +1,4 @@
-from .models import UserProfile
+from .models import UserProfile, AdminProfile
 from django.contrib.auth.models import User
 from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
@@ -12,20 +12,24 @@ from django.dispatch import receiver
 @receiver(post_save, sender=User)
 def create_Userprofile(sender, instance, created, **kwargs):
 
-    if created:
-        UserProfile.objects.create(user=instance)
-        print('UserProfile created')
+    if ((created) and (instance.is_staff)):
+        AdminProfile.objects.create(user=instance)
+        print('AdminProfile created')
+    else:
+        if (created):
+            UserProfile.objects.create(user=instance)
+            print('UserProfile created')
 
 
 # forma de conectar el User con el profile
-#post_save.connect(create_profile, sender=User)
+# post_save.connect(create_profile, sender=User)
 
 # @receiver(post_save, sender=User)
 # def update_Userprofile(sender, instance, created, **kwargs):
 
  #   if created == False:
-   #     UserProfile.save()
-  #      print('Userprofile updated')
+ #     UserProfile.save()
+#      print('Userprofile updated')
 
 
-#post_save.connect(update_profile, sender=User)
+# post_save.connect(update_profile, sender=User)
