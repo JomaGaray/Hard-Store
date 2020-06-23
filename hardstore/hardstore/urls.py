983f6424e31aa89edbb8e0185dee7d185a811aec
José Garay <josemgaray14@gmail.com>
2020-06-23 20:26:21 -0300

    Limpio un poco Urls

--- hardstore/hardstore/urls.py
+++ hardstore/hardstore/urls.py
@@ -26,13 +26,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', market_views.index.as_view(), name='home'),
 
+    # autenticacion
     path('accounts/', include('django.contrib.auth.urls')),
     # para el 'login_url' de las vistas, necesito hacer un url especifico, esto esta en la docu
     path('accounts/login/', include('django.contrib.auth.urls'), name='login'),
-    #  path('accounts/login/<next>', include('django.contrib.auth.urls'), name='login'),
 
-
-
+    # signUp de distintos Usuarios
     path('signup/CommonUser/',
          user_views.CommonUserSignUpView.as_view(), name='signup'),
     path('signup/ManagerUser/',
@@ -40,57 +39,32 @@ urlpatterns = [
     path('signup/ExecutiveUser/',
          user_views.ExecutiveUserSignUpView.as_view(), name='signupExecutiveUser'),
 
-
-    # path('', include('market.urls')), manera menos directa -Joma
-    #path('login/', user_views.VistaLogin.as_view(), name='login'),
-    #path('signup/', user_views.VistaSignup.as_view(), name='signup'),
-
+    # Busqueda de productos
     path('search/', market_views.SearchView.as_view(), name='search'),
-
-    # path para un prodcuto en particular DEL LADO DEL CLIENTE, un path dinamico
-    #path('producto/<int:pk_producto>/', market_views.VistaUnProducto.as_view(), name='producto'),
-
     path('producto/<int:pk_producto>/',
          market_views.ProductoDetail.as_view(), name='producto-detalle'),
-
     path('productos_categoria/<int:pk_categoria>',
          market_views.ProductosCategoriaList.as_view(), name='productos-categoria-list'),
 
 
-
-    # PATH CRUD hay que pasarlo cuando creemos todo la parte de administradores
-    # crear un producto
+    # -------Administracion-------
     path('crear_producto/', market_views.ProductoCreate.as_view(),
          name='crear_producto'),
 
-    # modificar un producto
-    #path('modificar_producto/<int:id>/', market_views.VistaCRUDProducto.ModProducto , name='modificar_producto'),
     path('modificar_producto/<int:pk>/',
          market_views.ProductoUpdate.as_view(), name='modificar_producto'),
 
-    # eliminar un producto
     path('eliminar_producto/<int:pk>/',
          market_views.ProductoDelete.as_view(), name='eliminar_producto'),
 
-    # PATH CRUD para Categorias
-
-    # crear una Categoria
     path('crear_categoria/', market_views.CategoriaCreate.as_view(),
          name='crear_categoria'),
 
-    # modificar un Categoria
     path('modificar_categoria/<int:pk>/',
          market_views.CategoriaUpdate.as_view(), name='modificar_categoria'),
 
-    # eliminar un Categoria
     path('eliminar_categoria/<int:pk>/',
          market_views.CategoriaDelete.as_view(), name='eliminar_categoria'),
-
-
-    # crear admin, path de prueba
-    #path('crearAdmin/', user_views.AdminSignUpView.as_view(), name='crear_Admin'),
-
-
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
